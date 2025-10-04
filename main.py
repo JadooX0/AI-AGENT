c6378b38b601584d05114b387ec4b8d688a84284
Navya <navyamedak_ece_25a12039@dtu.ac.in>
2025-10-04 23:14:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 from pydantic import BaseModel
-from langchain_openai import ChatOpenAI
+from langchain_grok import grok
 from langchain_anthropic import ChatAnthropic
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
